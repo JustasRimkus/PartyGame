14a26e1136492374ca61cd03e4cb802e2ae65fca
Justas Rimkus <jusrim3@ktu.lt>
2021-03-17 23:15:36 +0200

    fix

---
+++ Database/DbInitializer.cs
@@ -0,0 +1,36 @@
+﻿using Database.Enums;
+using Database.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Database
+{
+
+    public static class DbInitializer
+    {
+        public static void Initialize(PartyGameContext context)
+        {
+            context.Database.EnsureCreated();
+
+            if (context.Users.Any())
+            {
+                return;
+            }
+
+            var users = new User[]
+            {
+                new User { Username = "Maintainer", Email="maintianer@partygame.com", Hash="hash", Role = Role.Admin }
+            };
+
+            foreach (var user in users)
+            {
+                context.Users.Add(user);
+            }
+            context.SaveChanges();
+        }
+    }
+}
+
---
+++ Database/PartyGameContext.cs
@@ -0,0 +1,16 @@
+﻿using Database.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+
+namespace Database
+{
+    public class PartyGameContext : DbContext
+    {
+        public DbSet<User> Users { get; set; }
+
+        public PartyGameContext(DbContextOptions<PartyGameContext> options) : base(options)
+        {
+
+        }
+    }
+}
