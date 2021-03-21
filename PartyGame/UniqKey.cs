d0cf34f1c30dab5286f622e6e23278812953e8b4
LAPTOP-OQI19CRR\bmont <b.montvydas@gmail.com>
2021-03-21 13:36:34 +0200

    UniqKey class

---
+++ PartyGame/UniqKey.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GenerateRandomUniqKey
+{
+    public class UniqKey
+    {
+        private static Random random = new Random();
+
+        static public string GenerateKey()
+        {
+            Random random = new Random((int)DateTime.Now.Ticks);
+            const string pool = "QWERTYUIOPASDFGHJKLZXCVBNM0123456789";
+            StringBuilder builder = new StringBuilder();
+            for (int i = 0; i < 6; i++)
+            {
+                var ch = pool[random.Next(0, pool.Length)];
+                builder.Append(ch);
+            }
+            return builder.ToString();
+        }
+    }
+}
