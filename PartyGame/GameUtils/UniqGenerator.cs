e4e6396e0f738b684e94f0c3fc3dbc8acb785df1
LAPTOP-OQI19CRR\bmont <b.montvydas@gmail.com>
2021-03-21 14:11:55 +0200

    UniqKey

--- PartyGame/GameUtils/UniqGenerator.cs
+++ PartyGame/GameUtils/UniqGenerator.cs
@@ -4,16 +4,27 @@ using System.Text;
 
 namespace PartyGame
 {
-    public class UniqKey
+    public static class UniqGenerator
     {
-        private static Random random = new Random();
-
         static public string GenerateKey()
         {
             Random random = new Random((int)DateTime.Now.Ticks);
             const string pool = "QWERTYUIOPASDFGHJKLZXCVBNM0123456789";
             StringBuilder builder = new StringBuilder();
             for (int i = 0; i < 6; i++)
+            {
+                var ch = pool[random.Next(0, pool.Length)];
+                builder.Append(ch);
+            }
+            return builder.ToString();
+        }
+
+        static public string GenerateName() 
+        {
+            Random random = new Random((int)DateTime.Now.Ticks);
+            const string pool = "0123456789";
+            StringBuilder builder = new StringBuilder("CoolName");
+            for (int i = 0; i < 3; i++)
             {
                 var ch = pool[random.Next(0, pool.Length)];
                 builder.Append(ch);
