7e3dfc69755b2f7eaa4fdcb3d0ff35419749d4ec
LAPTOP-OQI19CRR\bmont <b.montvydas@gmail.com>
2021-04-09 13:46:32 +0300

    Add button, textbox, to index page. Added new method generate username to UniqGenerator class.

--- PartyGame/GameUtils/UniqGenerator.cs
+++ PartyGame/GameUtils/UniqGenerator.cs
@@ -4,14 +4,27 @@ using System.Text;
 
 namespace PartyGame
 {
-    public static class UniqGenerator
+    public class UniqGenerator
     {
-        static public string GenerateKey()
+        public string GenerateKey()
         {
             Random random = new Random((int)DateTime.Now.Ticks);
             const string pool = "QWERTYUIOPASDFGHJKLZXCVBNM0123456789";
             StringBuilder builder = new StringBuilder();
             for (int i = 0; i < 6; i++)
+            {
+                var ch = pool[random.Next(0, pool.Length)];
+                builder.Append(ch);
+            }
+            return builder.ToString();
+        }
+
+        public string GenerateUsername() 
+        {
+            Random random = new Random((int)DateTime.Now.Ticks);
+            const string pool = "0123456789";
+            StringBuilder builder = new StringBuilder("CoolNickname");
+            for (int i = 0; i < 3; i++)
             {
                 var ch = pool[random.Next(0, pool.Length)];
                 builder.Append(ch);
