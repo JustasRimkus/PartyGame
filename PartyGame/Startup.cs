a5ff36b54933b6b965ab95e41a4a37af170c3ec8
Griske24 <justas.griskevicius@gmail.com>
2021-04-11 19:42:35 +0300

    Added Blazor.ContextMenu package to the project. Added avatars to wwwroot folder. Created ProfileAvatar.razor page

--- PartyGame/Startup.cs
+++ PartyGame/Startup.cs
@@ -36,6 +36,7 @@ namespace PartyGame
             services.AddRazorPages();
             services.AddServerSideBlazor();
             services.AddSingleton<WeatherForecastService>();
+            services.AddBlazorContextMenu();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
