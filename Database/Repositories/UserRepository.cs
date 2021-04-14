79addf857fe2394e15d7521c29c630ba004f2cc9
Justas Rimkus <jusrim3@ktu.lt>
2021-04-14 14:37:09 +0300

    added repo

---
+++ Database/Repositories/UserRepository.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Database.Models;
+
+namespace Database.Repositories
+{
+    public class UserRepository
+    {
+        private readonly PartyGameContext dbContext;
+
+        public UserRepository(PartyGameContext dbContext)
+        {
+            this.dbContext = dbContext;
+        }
+
+        public async Task SaveAsync()
+        {
+            await dbContext.SaveChangesAsync();
+        }
+
+        public async Task AddAsync(User user)
+        {
+            await dbContext.Users.AddAsync(user);
+        }
+
+        public void Remove(User user)
+        {
+            dbContext.Users.Remove(user);
+        }
+
+        public void Update(User user)
+        {
+            dbContext.Users.Update(user);
+        }
+    }
+}
